1330245e7b0e28adcfca5d79a0cfa1b0414ca605
Nick Carducci <nmcarducci@gmail.com>
2022-08-07 17:33:12 -0400

    Update [[path]].js

--- functions/bear/[[path]].js
+++ functions/bear/[[path]].js
@@ -1,38 +1,47 @@
+import React from "react";
 //https://developers.cloudflare.com/pages/how-to/refactor-a-worker-to-pages-functions/
-export default {
+const Bear = {
   async fetch(r, env) {
     const url = new URL(r.url);
-    if (url.pathname.startsWith('/bear')) {
-      
-      /*const http = { Origin: r.headers.get("Origin") };
-      if (r.method === "OPTIONS") {//possibly redundant with Web App Firewall for page
-        return http.Origin.includes("sausage.saltbank.org")
-          ? new Response("yeah alright", {
-              status: 200,
-              statusText: "ok",
-              headers: {
-                "Content-Type": "Application/JSON",
-                "Access-Control-Allow-Origin": http.Origin,
-                "Access-Control-Allow-Methods": r.headers.get(
-                  "Access-Control-Request-Method"
-                ) //rq.headers["Access-Control-Request-Method"]
-                //...Object.keys(JSON.parse(rq.headers))
-              }
-              //"sausage.saltbank.org",
-            })
-          : new Response("no access for " + http.Origin, {
-              status: 403,
-              statusText: "not ok"
-              //"Access-Control-Allow-Origin": rq.http.Origin,
-              //"sausage.saltbank.org",
-            });
-      }*/
-      //return await fetch(new Request("https://sausage.saltbank.org/api/",r));//.then(async a=>await a.json());
-      //return await env.BANK.fetch(new Request("./api/", r));
-      return new Response('Ok');
-    }
+    //if (url.pathname.startsWith("/bear")) {
+    /*const http = { Origin: r.headers.get("Origin") };
+          if (r.method === "OPTIONS") {//possibly redundant with Web App Firewall for page
+            return http.Origin.includes("sausage.saltbank.org")
+              ? new Response("yeah alright", {
+                  status: 200,
+                  statusText: "ok",
+                  headers: {
+                    "Content-Type": "Application/JSON",
+                    "Access-Control-Allow-Origin": http.Origin,
+                    "Access-Control-Allow-Methods": r.headers.get(
+                      "Access-Control-Request-Method"
+                    ) //rq.headers["Access-Control-Request-Method"]
+                    //...Object.keys(JSON.parse(rq.headers))
+                  }
+                  //"sausage.saltbank.org",
+                })
+              : new Response("no access for " + http.Origin, {
+                  status: 403,
+                  statusText: "not ok"
+                  //"Access-Control-Allow-Origin": rq.http.Origin,
+                  //"sausage.saltbank.org",
+                });
+          }*/
+    const request = new Request(r, {
+      headers: { Authorization: `bearer ${env.CF_API_TOKEN}` }
+    });
+    return await fetch(
+      new Request("https://sausage.saltbank.org/api/", request)
+    ); //.then(async a=>await a.json());
+    //return await env.BANK.fetch(new Request("./api/", r));
+    //return new Response("Ok");}
+
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
-    return env.ASSETS.fetch(r);
-  },
+    //return env.ASSETS.fetch(r);
+  }
 };
+
+export default React.forwardRef((props, ref) => {
+  return <Bear fetch={ref.current["bear"]} {...props} />;
+});
