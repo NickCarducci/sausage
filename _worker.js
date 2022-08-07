fc1521301ef5b0de7dba8bbf51a591abc2611573
Nick Carducci <nmcarducci@gmail.com>
2022-08-07 09:20:37 -0400

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,37 @@
+//https://developers.cloudflare.com/pages/how-to/refactor-a-worker-to-pages-functions/
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/bear')) {
+      
+      const http = { Origin: r.headers.get("Origin") };
+      if (r.method === "OPTIONS") {//possibly redundant with Web App Firewall for page
+        return http.Origin.includes("sausage.saltbank.org")
+          ? new Response("yeah alright", {
+              status: 200,
+              statusText: "ok",
+              headers: {
+                "Content-Type": "Application/JSON",
+                "Access-Control-Allow-Origin": http.Origin,
+                "Access-Control-Allow-Methods": r.headers.get(
+                  "Access-Control-Request-Method"
+                ) //rq.headers["Access-Control-Request-Method"]
+                //...Object.keys(JSON.parse(rq.headers))
+              }
+              //"sausage.saltbank.org",
+            })
+          : new Response("no access for " + http.Origin, {
+              status: 403,
+              statusText: "not ok"
+              //"Access-Control-Allow-Origin": rq.http.Origin,
+              //"sausage.saltbank.org",
+            });
+      }
+      return await env.BANK.fetch(new Request("./api/", r));
+      //return new Response('Ok');
+    }
+    // Otherwise, serve the static assets.
+    // Without this, the Worker will error and no assets will be served.
+    return env.ASSETS.fetch(request);
+  },
+};
