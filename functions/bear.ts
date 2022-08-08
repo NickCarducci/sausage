53f89afc785e8fa971d0f5283b8cbdc6572f39c5
Nick Carducci <nmcarducci@gmail.com>
2022-08-08 09:42:36 -0400

    Update bear.ts

--- functions/bear.ts
+++ functions/bear.ts
@@ -81,8 +81,9 @@ export const onRequestPost: PagesFunction<{}> = async ({
   const request = new Request(r, {
     //headers: { Authorization: `bearer ${env.CF_API_TOKEN}` }
   });
-  return new Response("posted");
+  //return new Response("posted");
   //return await fetch(new Request("https://sausage.saltbank.org/api/", request));
+  return await fetch("https://sausage.saltbank.org/api/");
   //.then(async (res) => await res.json())
   //.then((result) => JSON.stringify(result))
   /*.then(
