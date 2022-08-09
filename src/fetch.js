61a8278e3390f1536ffd0a693d8860e23a008c14
Nick Carducci <nmcarducci@gmail.com>
2022-08-09 11:39:44 -0400

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -35,20 +35,21 @@ export default class Fetch extends React.Component {
       headers: {
         //https://developers.cloudflare.com/workers/examples/cors-header-proxy/
         //Origin: "https://i7l8qe.csb.app",
-        "Access-Control-Request-Headers": ["Allow", "Origin"],
+        "Access-Control-Request-Headers": ["Allow", "Origin"]
         //"Referrer-Policy": "cross-origin",
         //https://developers.cloudflare.com/firewall/api/cf-firewall-rules/post/
         //"X-Auth-Email": "nmcarducci@gmail.com",
         //"X-Auth-Key": context.env.CF_API_KEY, // cloudflare pages>settings>environment_variables
-        "Content-Type": "Application/JSON",
-        "Access-Control-Request-Method": "POST"
+
+        //"Content-Type": "Application/JSON",
+        //"Access-Control-Request-Method": "POST"
       },
-      method: "POST",
-      body: JSON.stringify({
+      method: "GET",
+      /*body: JSON.stringify({
         pageOffset: "0",
         pageLength: "10",
         postalCode: "77777"
-      }),
+      }),*/
       maxAge: 3600
     })
       .then(async (res) => await res.text())
